c15eea255bb4c27aebec1e7666d524ab7f235ebc
Srinu-7 <srinumaka9908@gmail.com>
2025-02-06 10:34:08 +0530

    url changed

--- src/config/api.js
+++ src/config/api.js
@@ -4,7 +4,7 @@ const DEPLOYED='https://e-commerce-springboot-production.up.railway.app'
 
 const LOCALHOST='http://localhost:8080'
 
-export const API_BASE_URL = LOCALHOST;
+export const API_BASE_URL = DEPLOYED;
 
 const api = axios.create({
   baseURL: API_BASE_URL,
